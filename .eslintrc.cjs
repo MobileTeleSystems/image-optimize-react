fa936aa29140af67244401fabca7974cb6b5958f
Eugene Labutin <labeg@mail.ru>
2025-07-19 16:07:56 +0300

    chore: update eslint configuration and upgrade devDependencies
    - Changed eslint configuration file from .eslintrc.cjs to eslint.config.js
    - Upgraded devDependencies:
      - @labeg/code-style from ^2.0.51 to ^6.5.0
      - @types/node from ^18.11.9 to ^24.0.15
      - @types/react from ^18.0.25 to ^19.1.8
      - husky from ^8.0.2 to ^9.1.7
      - lint-staged from ^13.0.3 to ^16.1.2
      - typescript from ^4.9.3 to ^5.8.3

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: ["./node_modules/@labeg/code-style/.eslintrc.js"],
-    ignorePatterns: ["node_modules/*"],
-    rules:{
-        // override here
-        "react/jsx-no-bind": "off",
-        "react/destructuring-assignment": "off",
-    }
-};
---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import labegStyle from "@labeg/code-style";
+
+/** @type {import("eslint").Linter.Config} */
+export default [
+    ...labegStyle,
+    {
+        rules: {
+            // custom rules can be added here
+        }
+    }
+];
